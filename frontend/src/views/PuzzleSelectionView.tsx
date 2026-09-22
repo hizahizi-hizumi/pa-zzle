a2337617163d5297a80be46f390967cc524324bd
hizahizi-hizumi <163728895+hizahizi-hizumi@users.noreply.github.com>
2026-09-22 23:27:09 +0900

    feat(frontend): マインスイーパーをゲーム選択へ追加

--- frontend/src/views/PuzzleSelectionView.tsx
+++ frontend/src/views/PuzzleSelectionView.tsx
@@ -1,4 +1,5 @@
 import { GameSelectionGallery } from "@/components/GameSelectionGallery";
+import minesweeperPictogramSvg from "@/games/minesweeper/assets/pictogram.svg?raw";
 import nanpurePictogramSvg from "@/games/nanpure/assets/pictogram.svg?raw";
 import waterSortPictogramSvg from "@/games/water-sort/assets/pictogram.svg?raw";
 
@@ -12,6 +13,11 @@ const games = [
     name: "ナンプレ",
     pictogramSvg: nanpurePictogramSvg,
     to: "/puzzles/nanpure",
+  },
+  {
+    name: "マインスイーパー",
+    pictogramSvg: minesweeperPictogramSvg,
+    to: "/puzzles/minesweeper/play",
   },
 ] as const;
 
